c975806f95b48564eb759eef9549e372bb5d62a5
chrisliu <383883040.qq.com>
2018-04-01 13:19:26 +0800

    第二次提交、

--- demo_01/index.js
+++ demo_01/index.js
@@ -1,1 +1,9 @@
 let http = require("http");
+let fs = require("fs");
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+}).listen(3000, () => {
+    console.log('start');
+});
