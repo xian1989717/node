f67c5ac5a876d966bd158b900590dc1bb8246989
chrisliu <383883040.qq.com>
2018-04-01 15:18:55 +0800

    第二次提交

--- demo_01/index.js
+++ demo_01/index.js
@@ -2,8 +2,26 @@ let http = require("http");
 let fs = require("fs");
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
+    if ('GET' == req.method && '/images' == req.url.substr(0, 7) && '.jpg' == req.url.substr(-4)) {
+        fs.stat(__dirname + req.url, (err, stat) => {
+            if (err || !stat.isFile()) {
+                res.writeHead(404);
+                res.end('Not found');
+                return;
+            }
+            serve(__dirname + req.url, 'application/jpg');
+        });
+    } else if ('GET' == req.method && '/' == req.url) {
+        serve(__dirname + '/index.html', 'text/html');
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
+    }
+
+    function serve(path, type) {
+        res.writeHead(200, { 'Content-Type': type });
+        fs.createReadStream(path).pipe(res);
+    }
 }).listen(3000, () => {
     console.log('start');
 });
